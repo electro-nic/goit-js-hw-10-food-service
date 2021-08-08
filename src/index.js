0295122027c81ae1d5468c2129def7e67a316d9e
YEVHEN BATSMAN <evgenbatsman@gmail.com>
2021-08-08 15:28:33 +0300

    update js + markup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-import './sass/main.scss';
+import menuElement from './menu.json';
+import menuTemplate from './templates/template.hbs';
+import './theme.js';
+import './styles.css';
+
+console.log(menuTemplate(menuElement[0]));
+
+
+
+let menu = document.querySelector('.js-menu');
+
+function buildMenu(array) {
+    const markup = array.map(post => menuTemplate(post)).join('');
+    menu.insertAdjacentHTML('beforeend', markup);
+}
+
+buildMenu(menuElement);
